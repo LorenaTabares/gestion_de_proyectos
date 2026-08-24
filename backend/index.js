52cbab5ebe51c8d79cc795339496b5d9e470d928
Ximena Cardona <ximena.cardona985@pascualbravo.edu.co>
2026-08-23 22:48:21 -0500

    feat: Conexión a base de datos y maquetacion del catalogo (#6, #7)

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,81 @@
+require('dotenv').config();
+
 const express = require('express');
+const path = require('path');
+
+const CatalogoController =
+    require('./controllers/catalogoController');
+
 const app = express();
-const PORT = 3000;
 
-// Esto permite que el backend entienda los datos JSON que envíe el frontend
-app.use(express.json());
 
-// Ruta de prueba para saber si el framework funciona
-app.get('/', (req, res) => {
-    res.send('¡El servidor de Express está funcionando perfectamente!');
-});
+// =====================================================
+// ARCHIVOS ESTÁTICOS
+// =====================================================
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.use(
+    express.static(
+        path.join(__dirname, '../frontend')
+    )
+);
+
+
+// =====================================================
+// EJS
+// =====================================================
+
+app.set(
+    'view engine',
+    'ejs'
+);
+
+app.set(
+    'views',
+    path.join(
+        __dirname,
+        '../frontend/html'
+    )
+);
+
+
+// =====================================================
+// RUTA CATÁLOGO
+// =====================================================
+
+app.get(
+    '/catalogo',
+    CatalogoController.mostrarCatalogo
+);
+
+
+// =====================================================
+// RUTA PRINCIPAL
+// =====================================================
+
+app.get(
+    '/',
+    (req, res) => {
+
+        res.redirect('/catalogo');
+
+    }
+);
+
+
+// =====================================================
+// SERVIDOR
+// =====================================================
+
+const PORT =
+    process.env.PORT || 3000;
+
+app.listen(
+    PORT,
+    () => {
+
+        console.log(
+            `Servidor corriendo en http://localhost:${PORT}`
+        );
+
+    }
+);
